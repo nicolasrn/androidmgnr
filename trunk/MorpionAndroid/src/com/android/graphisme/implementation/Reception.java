6f4798cd0185189c19b349be833c578fbbb0bd0f
nicolasrnp@gmail.com <nicolasrnp@gmail.com@320d592b-7a25-3434-640f-f41c3622773f>
2011-12-18 14:21:02 +0000

    erreur a corriger : - les pions (toujours le meme type s'affiche) - le logo (au niveau du bandeau de présentation, dans le jeu c'est ok)------>ok - interface de connexion réseau (signaler l'attente correctement) --------->ok - signaler les différentes issues du jeu (victoire, défaite, nul)------------->ok - régler le pb de la sortie avant affichage issu - enregistrer les résultats dans bdd - afficher historique des résultats

--- trunk/MorpionAndroid/src/com/android/graphisme/implementation/Reception.java
+++ trunk/MorpionAndroid/src/com/android/graphisme/implementation/Reception.java
@@ -2,6 +2,9 @@ package com.android.graphisme.implementation;
 
 import java.io.IOException;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
@@ -53,31 +56,79 @@ public class Reception extends Thread
 			}
 			else if (ordre.equals("win"))
 			{
+				/*AlertDialog.Builder builder = new AlertDialog.Builder(f.getContext());
+				builder.setMessage("Vous avez gagnŽ !!!")
+				       .setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+				           public void onClick(DialogInterface dialog, int id) {
+				                dialog.cancel();
+				           }
+				       });
+				AlertDialog alert = builder.create();
+				alert.show();*/
+				Message msg = handler.obtainMessage();
+                msg.arg1 = 2;
+                handler.sendMessage(msg);
 				//JOptionPane.showMessageDialog(f, "Vous avez gagné", "Resutat", JOptionPane.INFORMATION_MESSAGE);
 				//JOptionPane.showMessageDialog(f, f.getIterpret().reception_historique(), "Historique", JOptionPane.INFORMATION_MESSAGE);
-				System.exit(0);
+				//System.exit(0);
 			}
 			else if (ordre.equals("lose"))
 			{
+				/*AlertDialog.Builder builder = new AlertDialog.Builder(f.getContext());
+				builder.setMessage("Vous avez perdu ...")
+			       .setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			                dialog.cancel();
+			           }
+			       });
+				AlertDialog alert = builder.create();
+				alert.show();*/
+				Message msg = handler.obtainMessage();
+                msg.arg1 = 3;
+                handler.sendMessage(msg);
 				//JOptionPane.showMessageDialog(f, "Vous avez perdu", "Resutat", JOptionPane.INFORMATION_MESSAGE);
 				//JOptionPane.showMessageDialog(f, f.getIterpret().reception_historique(), "Historique", JOptionPane.INFORMATION_MESSAGE);
-				System.exit(0);
+				//System.exit(0);
 			}
 			else if (ordre.equals("matchnull"))
 			{
+				/*AlertDialog.Builder builder = new AlertDialog.Builder(f.getContext());
+				builder.setMessage("Pas de bol :p")
+				       .setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+				           public void onClick(DialogInterface dialog, int id) {
+				                dialog.cancel();
+				           }
+				       });
+				AlertDialog alert = builder.create();
+				alert.show();*/
+				Message msg = handler.obtainMessage();
+                msg.arg1 = 4;
+                handler.sendMessage(msg);
 				//JOptionPane.showMessageDialog(f, "Feliciation Match Null", "Resutat", JOptionPane.INFORMATION_MESSAGE);
 				//accusé de reception pour dire que le joueur ci est le gagnant
 				//JOptionPane.showMessageDialog(f, f.getIterpret().reception_historique(), "Historique", JOptionPane.INFORMATION_MESSAGE);
-				System.exit(0);
+				//System.exit(0);
 			}
 			else if (ordre.equals("decodeco"))
 			{
+				/*AlertDialog.Builder builder = new AlertDialog.Builder(f.getContext());
+				builder.setMessage("Victoire par abandon")
+				       .setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+				           public void onClick(DialogInterface dialog, int id) {
+				                dialog.cancel();
+				           }
+				       });
+				AlertDialog alert = builder.create();
+				alert.show();*/
+				Message msg = handler.obtainMessage();
+                msg.arg1 = 5;
+                handler.sendMessage(msg);
 				//JOptionPane.showMessageDialog(f, "l'autre joueur est deconnecté", "Resutat", JOptionPane.INFORMATION_MESSAGE);
 				//JOptionPane.showMessageDialog(f, f.getIterpret().reception_historique(), "Historique", JOptionPane.INFORMATION_MESSAGE);
 				//accusé de reception pour dire que le joueur ci est le gagnant
 				f.getIterpret().envoi_serveur("deco");
 				
-				System.exit(0);
+				//System.exit(0);
 			}
 			else
 				c = new Coordonnee(ordre);
@@ -85,7 +136,7 @@ public class Reception extends Thread
 			if (c != null)
 			{
                 Message msg = handler.obtainMessage();
-                msg.arg1 = 2;
+                msg.arg1 = 6;
                 msg.obj = c;
                 handler.sendMessage(msg);
 				//f.getGrille().getObjetTableauAt(c.x, c.y).activerImage(MImage.img1);
@@ -121,6 +172,9 @@ class GuiHandler extends Handler
 	@Override
 	public void handleMessage(Message msg) {
 		Log.v(MorpionAndroidActivity.tag, "je suis la c'est dŽja pas si mal");
+		AlertDialog.Builder builder;
+		AlertDialog alert;
+		
 		switch(msg.arg1)
 		{
 		case 1:
@@ -128,6 +182,50 @@ class GuiHandler extends Handler
 			f.getGrille().activerGrille();
 			break;
 		case 2:
+			builder = new AlertDialog.Builder(f.getContext());
+			builder.setMessage("Vous avez gagnŽ !!!")
+			       .setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			                dialog.cancel();
+			           }
+			       });
+			alert = builder.create();
+			alert.show();
+			break;
+		case 3:
+			builder = new AlertDialog.Builder(f.getContext());
+			builder.setMessage("Vous avez perdu ...")
+		       .setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                dialog.cancel();
+		           }
+		       });
+			alert = builder.create();
+			alert.show();
+			break;
+		case 4:
+			builder = new AlertDialog.Builder(f.getContext());
+			builder.setMessage("Pas de bol :p")
+			       .setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			                dialog.cancel();
+			           }
+			       });
+			alert = builder.create();
+			alert.show();
+			break;
+		case 5:
+			builder = new AlertDialog.Builder(f.getContext());
+			builder.setMessage("Victoire par abandon")
+			       .setCancelable(false).setPositiveButton("ok", new DialogInterface.OnClickListener() {
+			           public void onClick(DialogInterface dialog, int id) {
+			                dialog.cancel();
+			           }
+			       });
+			alert = builder.create();
+			alert.show();
+			break;
+		case 6:
 			Coordonnee c = (Coordonnee)msg.obj;
 			f.getGrille().getObjetTableauAt(c.x, c.y).activerImage(FenetreJeu.tabPion[(FenetreJeu.courant+1)%2]);
 			f.getGrille().activerGrille();
